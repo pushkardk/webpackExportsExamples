2367762db9ee72c2c6f7d6ea91740e4a009a99b4
Pushkar DK <pushkardk@hotstar.com>
2020-07-02 09:39:49 +0530

    some fixes

--- src/dynamic-imports.js
+++ src/dynamic-imports.js
@@ -1,11 +1,11 @@
 // Loading on demand:
 if (Math.random() > 0.5) {
   import('./dir2/a')
-    .then(module => console.log(module))
+    .then(module => console.log(module.aOne))
 }
 
 // Loading on demand with a magic comment:
 if (Math.random() > 0.5) {
   import(/* webpackChunkName: "module-b" */ './dir2/b')
-    .then(module => console.log(module))
+    .then(module => console.log(module.bTwo))
 }
